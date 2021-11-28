35e18c57d1e10a7c1a64c4f8f53ac2327d342e68
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-11-28 22:51:17 +0300

    adds the navbar

--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,5 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+def about(request):
+    return render(request,'about.html')
