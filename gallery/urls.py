6ec881e0ceaade96c354dea0f5ef1e04d51316f7
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-11-28 21:46:38 +0300

    adds the templates folder and welcome.html

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path
-from .import views
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
+urlpatterns=[
+    url('',views.welcome,name = 'welcome'),
 ]
-
