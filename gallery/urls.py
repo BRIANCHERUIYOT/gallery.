29e3b9369e9836b71f65f761e613af565e6e0e76
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-11-29 15:43:45 +0300

    adds about.html and change navbar

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,12 @@ from django.urls import path
 urlpatterns=[
     url('',views.welcome,name = 'welcome'),
     url('about/',views.about,name = 'about'),
-    path('search_category/',views.search_images, name='search_images')
+    url('search_category/',views.search_images, name='search_images')
+    # path("/", .as_view(), name="")('food/',views.school_images,name='food'),
+    # path('music/',views.nairobi_images,name='music'),
+    # path('hobbies/',views.coast_images,name='hobbies'),
+    # path('travel/',views.kericho_images,name='travel'),
+    # path('image_details/<int:image_id>',views.image_details,name='image.detail'),
+    # # path('search_category/',views.search_images, name='search_images')
     
 ]
