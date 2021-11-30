464d6d9d853fa877307deb1d5e3cca06a76d807c
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-11-30 22:15:04 +0300

    adds views

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,17 +1,22 @@
-from django.conf.urls import url
-from . import views
-from django.urls import path
+"""gallery URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
-urlpatterns=[
-    url('',views.welcome,name = 'welcome'),
-    url('about/',views.about,name = 'about'),
-    url('search_category/',views.search_images, name='search_images')
-    # path("/", .as_view(), name="")('food/',views.school_images,name='food'),
-    # path('music/',views.nairobi_images,name='music'),
-    # path('hobbies/',views.coast_images,name='hobbies'),
-    # path('travel/',views.kericho_images,name='travel'),
-    # path('image_details/<int:image_id>',views.image_details,name='image.detail'),
-    # # path('search_category/',views.search_images, name='search_images')
-    
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('images.urls'))
 ]
--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gall.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
